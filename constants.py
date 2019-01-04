bd108d8e6737d69e052e47bace936a10b532e9a5
Dave Packer <dave78packer@gmail.com>
2019-01-03 18:58:38 -0700

    pre branch commit

--- constants.py
+++ constants.py
@@ -1,7 +1,21 @@
 a_strategy = [
-    'AlwaysTrue',
-    'RandomDecision'
+    # 'AlwaysTrue',
+    # 'RandomDecision',
+    # 'DaveCopyCat',
+    # 'DaveCopyCat2',
+    # 'Dave1',
+    # 'Dave2',
+    # 'Dave3',
+    # 'Dave4',
+    # 'Dave5',
+    # 'Dave6',
+    # 'Dave7',
+    # 'Dave8',
+    # 'Dave9',
+    # 'DaveFinal',
+    # 'Dave37',
 ]
 
-GAME_COUNT = 3
+GAME_COUNT = 100000
+# DEBUG_ON = True
 DEBUG_ON = False
